8df591fba28f6b9b0731264dd23e857d5a4e0c1d
SebastianG03 <guamandavid11235@gmail.com>
2023-10-01 23:11:14 -0500

    Botones agregados parcialmente

--- Models/Producto.cs
+++ Models/Producto.cs
@@ -6,5 +6,27 @@
         public string Nombre { get; set;}
         public string Descripcion { get; set;}
         public int Cantidad { get; set;}
+
+        List<Producto> GenerarProductosAleatorios(int cantidad)
+        {
+            Random random = new Random();
+            List<Producto> productos = new List<Producto>();
+
+            for (int i = 1; i <= cantidad; i++)
+            {
+                Producto producto = new Producto
+                {
+                    IdProducto = i,
+                    Nombre = "Producto" + i,
+                    Descripcion = "Descripción del Producto " + i,
+                    Cantidad = random.Next(1, 101) // Cantidad aleatoria entre 1 y 100
+                };
+
+                productos.Add(producto);
+            }
+
+            return productos;
+        }
+        
     }
 }
